74c47c535366ce49b80b1bb847ef07961edcf95a
Sajdah Mustafa <132957619+sajilawaji@users.noreply.github.com>
2024-02-29 11:22:26 -0500

    progress 1

---
+++ index.js
@@ -0,0 +1,44 @@
+class Possessions {
+
+ constructor (sword, potion, artifact) {
+
+ this.sword = sword;
+ this.potion = potion;
+ this.artifact = artifact;
+
+
+ }
+
+}
+
+class Companion {
+
+    constructor(name, inventory, belongings) {
+
+
+        this.name = name 
+        this.inventory = inventory 
+        this.Possessions = possessions 
+    }
+
+
+}
+
+
+
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+
+    name:"Leo"
+    type: "Cat"
+  }
+
+};
+
+   const LeoeoPosessions = new Possessions ("small ")
+   const leo
+
+
